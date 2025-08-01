7a070ceb7e19102da317e64ccb11381f24e55bd4
bzsantosd <bdossantos626@gmail.com>
2025-08-01 16:44:10 -0300

    Atualizações

---
+++ Exemplo Script Modelo Escola.sql
@@ -0,0 +1,16 @@
+create database exemplo;
+
+use exemplo;
+
+create table aluno (
+id_aluno int primary key 
+);
+
+create table funcionario (
+id_funcionario int primary key 
+);
+
+-- cadastro aluno
+create table curso (
+id_curso int primary key 
+);
