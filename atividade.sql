b2b98b5376d60bc5b6fce6aebe9b1078d5df6f74
bzsantosd <bdossantos626@gmail.com>
2025-08-01 16:52:14 -0300

    Atividade

---
+++ atividade.sql
@@ -0,0 +1,17 @@
+create database atividade;
+
+use atividade;
+
+-- cadastro cliente
+
+create table cliente (
+id_cliente int primary key 
+);
+
+create table funcionario (
+id_funcionario int primary key 
+);
+
+create table pizzaria (
+id_pizzaria int primary key 
+);
