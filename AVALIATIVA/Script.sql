e886fd22dd68369591f8f5c789b03fbb120507a1
bzsantosd <bdossantos626@gmail.com>
2025-09-05 14:30:02 -0300

    Revisão 05.09

--- AVALIATIVA/Script.sql
+++ AVALIATIVA/Script.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE GerenciamentoProjetos;
 USE GerenciamentoProjetos;
+-- select database (); 
 
 CREATE TABLE Fornecedor (
     Fcodigo INT PRIMARY KEY,
     Fnome VARCHAR(255) NOT NULL,
-    Status VARCHAR(50),
-    Cidade VARCHAR(100)
+    Status VARCHAR(50) not null default 'ativo',
+    Cidade VARCHAR(100),
+    primary key (Fcodigo)
 );
 
 CREATE TABLE Peca (
@@ -13,12 +15,14 @@ CREATE TABLE Peca (
     Pnome VARCHAR(255) NOT NULL,
     Cor VARCHAR(50),
     Peso INT,
-    Cidade VARCHAR(100)
+    Cidade VARCHAR(100),
+    primary key (Pcodigo)
 );
 
 CREATE TABLE Instituicao (
     Icodigo INT PRIMARY KEY,
-    Nome VARCHAR(255) NOT NULL
+    Nome VARCHAR(255) NOT NULL,
+    primary key (Icodigo)
 );
 
 CREATE TABLE Projeto (
@@ -40,8 +44,25 @@ CREATE TABLE Fornecimento (
     FOREIGN KEY (PRcod) REFERENCES Projeto(PRcod)
 );
 
+SHOW TABLES; 
+
+show index from tbl_projeto;
+
+create index idx_icodigo on tbl_projeto(icodigo);
+
+SHOW TABLES; 
+select Fnome from tbl_fornecedor;
+select * from tbl_peca;
+
 ALTER TABLE Fornecedor
 ALTER COLUMN Status SET DEFAULT 'Ativo';
+
+-- alter table tbl_fornecedor
+-- add column ccod int not null;
+
+-- alter table tbl_fornecedor
+-- add constraint fk_ccod_fornecedor
+-- foreign key (ccod) references tbl_cidade (ccod);
 
 ALTER TABLE Fornecedor
 ADD Fone VARCHAR(20),
@@ -62,5 +83,17 @@ DROP COLUMN Icod,
 ADD Ccod INT,
 ADD FOREIGN KEY (Ccod) REFERENCES Cidade(Ccod);
 
+drop table tbl_instituicao;
+
 ALTER TABLE Fornecimento
 ADD COLUMN Quantidade INT;
+
+insert into tbl_cidade values (11, 'Limeira', 'SP');
+insert into tbl_cidade values (10, 'São Paulo', 'SP');
+insert into tbl_cidade values (19, 'Campinas', 'SP');
+insert into tbl_cidade values (15, 'Cordeiropolis', 'SP');
+
+-- verificar ultimo valor inserido de id
+ select last_insert_id();
+ 
+ 
