e0b18af5cb95c91ea2db4cc2693f96e191d92c1e
bzsantosd <bdossantos626@gmail.com>
2025-09-19 16:57:47 -0300

    19.09.2025

--- Letras_codigos/index1.php
+++ Letras_codigos/index1.php
@@ -12,7 +12,7 @@ $column = isset($_GET['column']) && in_array($_GET['column'], $columns) ? $_GET[
 $sort_order = isset($_GET['order']) && strtolower ($_GET['order']) == 'desc' ?'DESC' : 'ASC';
 
 //verificar dados no banco
-if ($result = $mysqli->query('select * from livros order by' . $column . '' . $sort_order)) {
+if ($result = $mysqli->query('select * from livros_vendas order by' . $column . '' . $sort_order)) {
     // varias para a tabela
     $up_or_down = str_replace(array('ASC','DESC'), array('up', 'down'), $sort_order);
     $asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
@@ -28,10 +28,24 @@ if ($result = $mysqli->query('select * from livros order by' . $column . '' . $s
     <body>
         <table>
             <tr>
-                <th><a href="index1.php?column=titulo&order=<?php echo $asc_or_desc; ?>">Título</th>
+                <th><a href="index1.php?column=titulo&order=<?php echo $asc_or_desc; ?>">Título <?php echo $column == 'titulo' ? '-' . $up_or_down : ''; ?> </th>
+
+                <th><a href="index1.php?column=data&order=<?php echo $asc_or_desc; ?>">Data <?php echo $column == 'data_venda' ? '-' . $up_or_down : ''; ?> </th>
+
+                <th><a href="index1.php?column=preco&order=<?php echo $asc_or_desc; ?>">Preco <?php echo $column == 'preco' ? '-' . $up_or_down : ''; ?> </th>
             </tr>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td <?php echo $column =='titulo' ? $add_class : ''; ?>> <?php echo $row ['titulo']; ?> </td>
+
+                     <td <?php echo $column =='data_venda' ? $add_class : ''; ?>> <?php echo $row ['data_venda']; ?> </td>
+
+                     <td <?php echo $column =='preco' ? $add_class : ''; ?>> <?php echo $row ['preco']; ?> </td>
+            </tr>
+                <?php endwhile;?>
         </table>
     </body>
     </html>
+    <?php $result->free();
 }
-
+?>
